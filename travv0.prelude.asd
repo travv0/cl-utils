6ab1be0c3c06a831e9c2bd5b18ddb236a4153022
Travis <tssund93@gmail.com>
2019-12-30 22:36:56 -0600

    added a prelude of packages i always use

---
+++ travv0.prelude.asd
@@ -0,0 +1,5 @@
+(asdf:defsystem #:travv0.prelude
+  :serial t
+  :depends-on (:alexandria :metabang-bind :travv0.utils :cl-arrows)
+  :pathname "./"
+  :components ((:file "prelude")))
--- utils.lisp
+++ utils.lisp
@@ -1,6 +1,6 @@
 (in-package :cl-user)
-(uiop:define-package :travv0.utils
-  (:nicknames :tu)
+(uiop:define-package #:travv0.utils
+    (:nicknames :tu)
   (:use #:cl #:alexandria)
   (:shadow cl:defun)
   (:export #:defun))
@@ -17,43 +17,43 @@
                        (append new-list '(&allow-other-keys))
                        new-list))
                  list)))
-    (let* ((expression (loop for param in lambda-list
-                             with into-keys = nil
-                             when (not (char= (char (write-to-string param) 0)
-                                              #\&))
-                               append (if into-keys
-                                          (if (listp param)
-                                              (if (listp (car param))
-                                                  (car param)
-                                                  (list (make-keyword
-                                                         (write-to-string (car param)))
-                                                        (intern (write-to-string (car param)))))
-                                              (list (make-keyword param) param))
-                                          (list (intern (string-upcase
-                                                         (write-to-string param)))))
-                             when (eql param '&key)
-                               do (setf into-keys t)))
+    (let* ((expression (loop :for param :in lambda-list
+                             :with into-keys = nil
+                             :when (not (char= (char (write-to-string param) 0)
+                                               #\&))
+                               :append (if into-keys
+                                           (if (listp param)
+                                               (if (listp (car param))
+                                                   (car param)
+                                                   (list (make-keyword
+                                                          (write-to-string (car param)))
+                                                         (intern (write-to-string (car param)))))
+                                               (list (make-keyword param) param))
+                                           (list (intern (string-upcase
+                                                          (write-to-string param)))))
+                             :when (eql param '&key)
+                               :do (setf into-keys t)))
            (db-lambda-list (remove-if (lambda (param)
                                         (and (char= (char (write-to-string param)
                                                           0)
                                                     #\&)
                                              (not (eql param '&key))))
                                       (add-allow-keys lambda-list)))
-           (defun-lambda-list (loop for param in lambda-list
-                                    with into-keys = nil
-                                    when (eql param '&key)
-                                      do (setf into-keys t)
-                                    append (if into-keys
-                                               (if (listp param)
-                                                   (list
-                                                    (if (listp (car param))
-                                                        param
-                                                        (list (intern
-                                                               (write-to-string (car param)))
-                                                              (second param))))
-                                                   (list param))
-                                               (list (intern (string-upcase
-                                                              (write-to-string param))))))))
+           (defun-lambda-list (loop :for param :in lambda-list
+                                    :with into-keys = nil
+                                    :when (eql param '&key)
+                                      :do (setf into-keys t)
+                                    :append (if into-keys
+                                                (if (listp param)
+                                                    (list
+                                                     (if (listp (car param))
+                                                         param
+                                                         (list (intern
+                                                                (write-to-string (car param)))
+                                                               (second param))))
+                                                    (list param))
+                                                (list (intern (string-upcase
+                                                               (write-to-string param))))))))
       `(cl:defun ,name (,@defun-lambda-list)
          (destructuring-bind ,db-lambda-list (list ,@expression)
            ,@body)))))
