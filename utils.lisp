11fe553b768f26fc2459f709e69741d9548ee1c3
Travis <tssund93@gmail.com>
2019-12-30 20:18:47 -0600

    always allow other keys in desfun when destructuring plists

--- utils.lisp
+++ utils.lisp
@@ -6,42 +6,52 @@
 (in-package :travv0.utils)
 
 (defmacro desfun (name lambda-list &body body)
-  (let ((expression (loop for param in lambda-list
-                          with into-keys = nil
-                          when (not (char= (char (write-to-string param) 0)
-                                           #\&))
-                            append (if into-keys
-                                       (if (listp param)
-                                           (if (listp (car param))
-                                               (car param)
-                                               (list (make-keyword
-                                                      (write-to-string (car param)))
-                                                     (intern (write-to-string (car param)))))
-                                           (list (make-keyword param) param))
-                                       (list (intern (string-upcase
-                                                      (write-to-string param)))))
-                          when (eql param '&key)
-                            do (setf into-keys t)))
-        (db-lambda-list (remove-if (lambda (param)
-                                     (and (char= (char (write-to-string param)
-                                                       0)
-                                                 #\&)
-                                          (not (eql param '&key))))
-                                   lambda-list))
-        (defun-lambda-list (loop for param in lambda-list
-                                 with into-keys = nil
-                                 when (eql param '&key)
-                                   do (setf into-keys t)
-                                 append (if into-keys
-                                            (if (listp param)
-                                                (if (listp (car param))
-                                                    (list param)
-                                                    (list (list (intern
-                                                                 (write-to-string (car param)))
-                                                                (second param))))
-                                                (list param))
-                                            (list (intern (string-upcase
-                                                           (write-to-string param))))))))
-    `(defun ,name (,@defun-lambda-list)
-       (destructuring-bind ,db-lambda-list (list ,@expression)
-         ,@body))))
+  (labels ((add-allow-keys (list)
+             (if (listp list)
+                 (let ((new-list (mapcar #'add-allow-keys list)))
+                   (if (and (eql (car new-list) '&key)
+                            (not (position '&allow-other-keys new-list)))
+                       (append new-list '(&allow-other-keys))
+                       new-list))
+                 list)))
+    (let* ((lambda-list (add-allow-keys lambda-list))
+           (expression (loop for param in lambda-list
+                             with into-keys = nil
+                             when (not (char= (char (write-to-string param) 0)
+                                              #\&))
+                               append (if into-keys
+                                          (if (listp param)
+                                              (if (listp (car param))
+                                                  (car param)
+                                                  (list (make-keyword
+                                                         (write-to-string (car param)))
+                                                        (intern (write-to-string (car param)))))
+                                              (list (make-keyword param) param))
+                                          (list (intern (string-upcase
+                                                         (write-to-string param)))))
+                             when (eql param '&key)
+                               do (setf into-keys t)))
+           (db-lambda-list (remove-if (lambda (param)
+                                        (and (char= (char (write-to-string param)
+                                                          0)
+                                                    #\&)
+                                             (not (eql param '&key))))
+                                      lambda-list))
+           (defun-lambda-list (loop for param in lambda-list
+                                    with into-keys = nil
+                                    when (eql param '&key)
+                                      do (setf into-keys t)
+                                    append (if into-keys
+                                               (if (listp param)
+                                                   (list
+                                                    (if (listp (car param))
+                                                        param
+                                                        (list (intern
+                                                               (write-to-string (car param)))
+                                                              (second param))))
+                                                   (list param))
+                                               (list (intern (string-upcase
+                                                              (write-to-string param))))))))
+      `(defun ,name (,@defun-lambda-list)
+         (destructuring-bind ,db-lambda-list (list ,@expression)
+           ,@body)))))
